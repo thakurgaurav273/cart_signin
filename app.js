a2cb015db1f675968866fb71cf96faeff32b5a95
gaurav singh <thakurgaurav273@gmail.com>
2023-02-19 12:36:33 +0530

    implemented signup by fb and google

--- app.js
+++ app.js
@@ -3,81 +3,170 @@ const express=require('express');
 const ejs=require('ejs');
 const bodyParser=require('body-parser');
 const mongoose=require("mongoose");
+const session=require('express-session');
+const passport=require('passport');
+const findOrCreate=require("mongoose-findorcreate");
+const passportLocalMongoose=require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-const bcrypt=require('bcrypt');
+
 const app=express();
+app.set("view engine","ejs");
 
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.set("view engine","ejs");
+app.use(session({
+  secret: 'Our little secret',
+  resave: false,
+  saveUninitialized: false
+}))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set('strictQuery',true);
 mongoose.connect(process.env.URL);
 
 
 const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    contact:Number,
-    dob:Date,
+    googleId:String,
+    username:String,
     password:String,
+    facebookId:String
 
 });
-const saltRounds=10;
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
-const user=new mongoose.model("User",userSchema);
+
+const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/cart"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id ,username:profile.displayName}, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_CLIENT_ID,
+    clientSecret: process.env.FB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/cart",
+    profileFields: ['id', 'displayName', 'photos', 'email'],
+  },
+ async function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id ,
+        username:profile.displayName
+    }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook',{scope:'email'}));
+
+app.get('/auth/facebook/cart',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/cart');
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/cart', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/cart');
+  });
+
+
+  
 app.get("/",function(req,res){
     res.render("home");
+}); 
+
+app.get("/login",function(req,res){
+    res.render("login");
 });
 
 app.get("/register",function(req,res){
     res.render("register");
 });
 
-app.get("/login",function(req,res){
-    res.render("login");
+app.get("/cart",function(req,res){
+    console.log(req.isAuthenticated());
+    if(req.isAuthenticated()){
+        res.render("cart");
+    }
+    else{
+        res.redirect("/login");
+    }
 });
 
-app.post("/register",function(request,response){
-    bcrypt.hash(request.body.password, saltRounds, function(err, hash) {
-        var email=request.body.email_id;
-        var user_name=request.body.name;
-        var user_dob=request.body.birthday;
-        var mob=request.body.mob_number;
-        const new_user=new user({
-            name:user_name,
-            email:email,
-            contact:mob,
-            dob:user_dob,
-            password:hash
-        });
-        new_user.save();
-        response.render("cart",{name:user_name});
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
     });
-    
+  });
+app.post("/register",function(request,response){
+    User.register({username:request.body.username},request.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            response.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(request,response,function(){
+                response.redirect("/cart");
+            });
+        }
+    })
 });
 
 app.post("/login",function(request,response){
-    var email=request.body.email_id;
-    user.findOne({email:email},function(err,foundUser){
+    const newUser=new User({
+        username:request.body.username,
+        password:request.body.password
+    })
+    request.login(newUser,function(err){
         if(err){
             console.log(err);
+
         }
         else{
-            if(foundUser){
-                bcrypt.compare(request.body.password, foundUser.password, function(err, result) {
-                    if(result===true){
-                        response.render("cart",{name:foundUser.name});
-                    }
-                    else{
-                        response.render("register");
-                    }
-                });
-            }
+            passport.authenticate("local")(request,response,function(){
+                response.redirect("/cart");
+            })
         }
-    });
+    })
+
 })
 
 
